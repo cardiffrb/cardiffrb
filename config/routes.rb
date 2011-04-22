22a5667c24b6059ae1ed49cb7de5ea5f38667141
Gareth Rees <gareth@garethrees.co.uk>
2011-04-22 21:37:02 +0100

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Cardiffrb::Application.routes.draw do
 
-  match "/auth/:provider/callback" => "sessions#create" 
+  match "/auth/:provider/callback" => "sessions#create"
+  
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,6 +54,7 @@ Cardiffrb::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
