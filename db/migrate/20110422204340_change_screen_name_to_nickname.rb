d6347aa7def51dffe81310a7085a2959cef67a79
Gareth Rees <gareth@garethrees.co.uk>
2011-04-22 21:47:15 +0100

    Migration for previous commit

--- db/migrate/20110422204340_change_screen_name_to_nickname.rb
+++ db/migrate/20110422204340_change_screen_name_to_nickname.rb
@@ -1,7 +1,13 @@
 class ChangeScreenNameToNickname < ActiveRecord::Migration
   def self.up
+    change_table :users do |t|
+      t.rename :screen_name, :nickname
+    end
   end
 
   def self.down
+    change_table :users do |t|
+      t.rename :nickname, :screen_name
+    end
   end
 end
