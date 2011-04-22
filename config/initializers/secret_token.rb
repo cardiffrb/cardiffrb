ab0e4d1bcb8de69b1c8ca0419dffe56a604077e0
Gareth Rees <gareth@garethrees.co.uk>
2011-04-22 20:38:20 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardiffrb::Application.config.secret_token = '5787e3d5a60cbb2f3c3d4e11647c53ecd8a74530fb1e986a6b090481147cea32cf23bcd35d74655f9dd88a575cc21d12427af9ae6b7226577608d5b74e4e08e0'
